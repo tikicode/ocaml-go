2a0725100abbc12f60706de8f9a75cc0e8c27935
ZheyueZhao <zheyuezhao@gmail.com>
2023-10-22 23:01:15 -0400

    place chess pieces

--- src/main.ml
+++ src/main.ml
@@ -17,22 +17,63 @@ let print_board board =
     match board with
     | [] -> ()
     | row :: rest ->
-      Printf.printf "%2d " row_number; 
+      Printf.printf "%2d " (row_number + 1);
       print_row row;
-      print_rows rest (row_number + 1) 
+      print_rows rest (row_number + 1)
   in
 
-  print_string "    "; (* Add extra space for alignment *)
+  print_string "    ";
   for i = 0 to board_size - 1 do
-    Printf.printf "%2d " (i + 1) (* Print the column number starting from 1 *)
+    Printf.printf "%2d " (i + 1)
   done;
   print_newline ();
   print_rows board 0
 
+let update_board board x y value =
+  if x < 0 || x >= board_size || y < 0 || y >= board_size then
+    board  (* Coordinates out of bounds, return the original board *)
+  else
+    let rec update_row row col value =
+      match row with
+      | [] -> []
+      | cell :: rest ->
+        if col = 0 then
+          value :: rest
+        else
+          cell :: update_row rest (col - 1) value
+    in
+    let rec update_rows rows row value =
+      match rows with
+      | [] -> []
+      | r :: rest ->
+        if row = 0 then
+          (update_row r y value) :: rest
+        else
+          r :: update_rows rest (row - 1) value
+    in
+    update_rows board x value
 
-(* Main game loop *)
-let game_loop board =
-  print_board board
+let rec game_loop board =
+  print_board board;
+  print_string "Enter the row and column (e.g., '2 2') to place 'B': ";
+  match read_line () with
+  | exception End_of_file -> ()
+  | input ->
+    match String.split_on_char ' ' input with
+    | [row_str; col_str] ->
+      begin
+        try
+          let row = int_of_string row_str in
+          let col = int_of_string col_str in
+          let updated_board = update_board board (row - 1) (col - 1) 'B' in
+          game_loop updated_board
+        with
+        | Failure _ ->
+          print_string "Invalid input. Please enter valid coordinates.\n";
+          game_loop board
+      end
+    | _ ->
+      print_string "Invalid input format. Please enter coordinates as 'row col'.\n";
+      game_loop board
 
 let () = game_loop initial_board
-  
