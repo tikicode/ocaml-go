645ce62659443f58f35735a55205532725fc775d
JohnDelucia <johndeluciabrunswick@gmail.com>
2023-11-29 15:54:27 -0500

    Implemented a post endpointment allowing frontend to make a move

--- _build/default/bin/dream_routes.ml
+++ _build/default/bin/dream_routes.ml
@@ -2,6 +2,7 @@
   Dream.run ~interface:"0.0.0.0" ~port:3000 (fun _ ->
     Dream.html "Hello world!") *)
 
+
 type data = {
   message: string;
 }
@@ -16,32 +17,26 @@ let api_handler _req =
   let data = { message = "Hello, Dream API!" } in
   Dream.json (Yojson.Safe.to_string (data_to_yojson data))
 
+let post_handler request =
+  let%lwt body = Dream.body request in
+  Dream.html (Printf.sprintf "Received POST request with body: %s" body)
+
+
 let () =
   Dream.run 
   @@ Dream.logger
   @@ Dream.router [
     Dream.get "/api" api_handler;
-
+  
+    (* adding parametrized url instead *)
     Dream.get "/api/:word"
       (fun request ->
         let content = (Dream.param request "word") in 
           match content with 
-          | "2 2" -> Dream.html "hello"
+          | "2_2" -> let data = { message = content } in Dream.json ((Yojson.Safe.to_string (data_to_yojson data)))
           | _ -> Dream.html "world"
       );
+    
+    Dream.post "/api/p" post_handler;
+
   ]
-(* 
-  let () =
-  Dream.run
-  @@ Dream.logger
-  @@ Dream.router [
-
-    Dream.get "/"
-      (fun _ ->
-        Dream.html "Good morning, world!");
-
-    Dream.get "/echo/:word"
-      (fun request ->
-        Dream.html (Dream.param request "word"));
-
-  ] *)
